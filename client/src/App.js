285220000aea8dba64d6ce9e15d39f43a929bdd8
Erik Nelson <erik.cse@gmail.com>
2020-07-06 17:59:23 -0700

    Basic react webapp
    - Ran create-react-app
    - Removed some unnecessary code

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hey look ma we made it.
+      </header>
+    </div>
+  );
+}
+
+export default App;
